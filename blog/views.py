f3250a27380a812390034b10c0405bef564f9fac
fahadals09 <fahadals1100@gmail.com>
2021-06-09 20:44:56 +0300

    add new

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def blog(request):
+    
+    
+
+    return HttpResponse('the page here')
