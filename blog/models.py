26345ac512d154540b47222dcc042899dc87cb44
fahadals09 <fahadals1100@gmail.com>
2021-06-11 21:20:08 +0300

    first line

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+GENDER =(
+    ('MALE' , 'MALE'),
+    ('FEMAIL' , 'FEMAIL')
+)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=2000)
+    active = models.BooleanField(default = False)
+    created_at = models.DateField(default=timezone.now)
+    edited_At = models.DateTimeField(default=timezone.now)
+    emil = models.EmailField(default= '')
+    gender = models.CharField(choices=GENDER , max_length=10)
